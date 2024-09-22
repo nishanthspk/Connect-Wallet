17774c4ca961b113c5780bdce30994c0df8ad821
nishanthspk <spknishanth2004@gmail.com>
2024-09-22 22:55:37 +0530

    eslint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+      <Navbar />  {/* Include Navbar here */}
       
+    </Router>
     </div>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import CustomButton from './Button';
+// import CustomButton from './Button';
 // import MobileMenu from '../Components/MobileView.jsx';
 import {  useNavigate  } from "react-router-dom";
 import { useState } from 'react'
 
 const Navbar = () => {
 //   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-//   const navigate = useNavigate();
+  const navigate = useNavigate();
 
 //   const toggleMobileMenu = () => {
 //     setIsMobileMenuOpen(!isMobileMenuOpen);
@@ -16,6 +16,8 @@ const Navbar = () => {
     <div className=' ' >
     <nav className=''>
             {/* <MobileMenu isOpen={isMobileMenuOpen} onClose={toggleMobileMenu} /> */}
+            
+            <div>connect</div>
     </nav>
     </div>
   );
